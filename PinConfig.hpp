ff3eb4a30261a77534faca1c3b54a307240320fe
meclondrej <ondrej.mecl@volny.cz>
2025-07-13 13:29:31 +0200

    Added the pin configuration for the shift register

---
+++ PinConfig.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define PIN_OE 3
+#define PIN_RCLK 4
+#define PIN_SRCLR 5
+#define PIN_SRCLK 6
+#define PIN_SER 7
--- display232ttl.ino
+++ display232ttl.ino
@@ -1,3 +1,12 @@
-void setup() {}
+#include <Arduino.h>
+#include "PinConfig.hpp"
+
+void setup() {
+    pinMode(PIN_OE, OUTPUT);
+    pinMode(PIN_RCLK, OUTPUT);
+    pinMode(PIN_SRCLR, OUTPUT);
+    pinMode(PIN_SRCLK, OUTPUT);
+    pinMode(PIN_SER, OUTPUT);
+}
 
 void loop() {}
